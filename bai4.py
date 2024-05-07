cc6be600e56096d420c664bee9735d2e2877e18d
ngocvu <ngocibaraki@gmail.com>
2024-05-07 22:22:55 +0900

    hoc ve list

---
+++ bai4.py
@@ -0,0 +1,7 @@
+danhBaDienThoai = ["ngoc", "son"]
+danhBaDienThoai[0]
+print(danhBaDienThoai[0])
+tenCacLoaiHoaQua = ["man","dao", "quyt"]
+print(tenCacLoaiHoaQua[2])
+quaMuaHe = tenCacLoaiHoaQua[0]
+print(quaMuaHe)
