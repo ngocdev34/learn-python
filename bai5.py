cba22b2ba9eea8f3c64a94ec63e8f15e3203d3cb
ngocvu <ngocibaraki@gmail.com>
2024-05-09 21:54:53 +0900

    hoc vong lap

---
+++ bai5.py
@@ -0,0 +1,20 @@
+cacSoChan = [2, 4, 6, 8]
+print(cacSoChan[0])
+print(cacSoChan[1])
+print(cacSoChan[2])
+print(cacSoChan[3])
+print(cacSoChan[0],cacSoChan[1],cacSoChan[2],cacSoChan[3])
+cacSo = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for x in cacSo : 
+    if x % 2 == 0:
+        print(x)
+
+print("------------")
+for x in cacSo : 
+    if x % 2 != 0:
+        print(x)
+print("------------")
+for x in cacSo : 
+    if x >= 15:
+        print(x)
+
